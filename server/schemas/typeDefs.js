3a86a6ef59f5c59dc3841a27c4894548d5feb5fb
Sajjadalgburi <dfgsajjad@gmail.com>
2024-06-25 15:02:41 -0600

    initilized the typeDefs for the application

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,3 +1,41 @@
 // this file will contain the typeDefinitons for the GraphQL API
 
-const gql = require("graphql-tag");
+const gql = require('graphql-tag');
+
+const typeDefs = gql`
+  ### Defined the User type
+  type User {
+    _id: ID
+    username: String!
+    email: String!
+    favourCryptos: [FavourCrypto]
+  }
+
+  type FavourCrypto {
+    _id: ID
+    name: String!
+    image: String!
+  }
+
+  input cryptoInput {
+    name: String!
+    image: String!
+  }
+
+  ## Defined the Auth type to validate the user
+  type Auth {
+    token: ID!
+    user: User
+  }
+
+  ## defineing the nescessary queries to be made
+  type Query {
+    me: User
+  }
+
+  type Mutation {
+    SaveCrypto(cryptoInput: cryptoInput!): User
+  }
+`;
+
+module.exports = typeDefs;
