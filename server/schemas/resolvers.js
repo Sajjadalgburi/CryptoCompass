7dcee312f505caa140c8724fddda0532860642ed
Sajjadalgburi <dfgsajjad@gmail.com>
2024-06-27 14:59:51 -0600

    created the login mutation to login the user with provided info

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -25,24 +25,43 @@ const resolvers = {
 
   // this will allow the user to preform CRUDE operations
   Mutation: {
-    createUser(parent, { username, email, password }) {
+    createUser: async (parent, { username, email, password }) => {
       try {
         // creating a new user with the provided args
-        const user = User.create({ username, email, password });
+        const user = await User.create({ username, email, password });
 
-        if (!user) {
-          throw new Error('cannot create a new user');
-        }
+        if (!user) throw new Error('cannot create a new user');
 
         // assigning the user a new JWT for authentication
         const token = signToken(user);
 
-        // returning the newly created user and JWT
         return { user, token };
       } catch (err) {
         // logging any error in the process
         console.error(err);
         throw new Error('cannot create a new user');
+      }
+    },
+    login: async (parent, { username, password }) => {
+      try {
+        // finding the user with the given username and also running a
+        // method to check if the provided password is correct
+        const user = await User.findOne({ username });
+        const isCorrectPassword = User.isCorrectPassword(password);
+
+        // running if statement to throw error when 1. the user doesn't exist 2. if the pass is incorrect
+        if (!user)
+          throw new Error(`Cannot find the user with username: ${username}`);
+        if (!isCorrectPassword) throw new Error('Password is incorrect');
+
+        // assigning the user a new JWT for authentication
+
+        const token = signToken(user);
+
+        return { user, token };
+      } catch (err) {
+        console.error(err);
+        throw new Error('failed to login!');
       }
     },
   },
