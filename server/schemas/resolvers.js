d4b5ed2dcd57fc59e4e503d5c9b1a456cbb090ff
Sajjadalgburi <dfgsajjad@gmail.com>
2024-06-27 14:38:04 -0600

    initilized the create user mutation

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -3,12 +3,13 @@ require('dotenv').config();
 
 // importing the User model
 const { User } = require('../models');
+const { signToken } = require('../utils/auth.js');
 
 const resolvers = {
   // this is read only data to retrieve data from the server
   Query: {
     // this will return the users data
-    me(__, args, context) {
+    me(parent, args, context) {
       try {
         if (context.user) {
           // find the ID of the user that is logged in
@@ -22,8 +23,29 @@ const resolvers = {
     },
   },
 
-  // this will allow the user to prefrom CRUDE operations
-  Mutation: {},
+  // this will allow the user to preform CRUDE operations
+  Mutation: {
+    createUser(parent, { username, email, password }) {
+      try {
+        // creating a new user with the provided args
+        const user = User.create({ username, email, password });
+
+        if (!user) {
+          throw new Error('cannot create a new user');
+        }
+
+        // assigning the user a new JWT for authentication
+        const token = signToken(user);
+
+        // returning the newly created user and JWT
+        return { user, token };
+      } catch (err) {
+        // logging any error in the process
+        console.error(err);
+        throw new Error('cannot create a new user');
+      }
+    },
+  },
 };
 
 module.exports = resolvers;
